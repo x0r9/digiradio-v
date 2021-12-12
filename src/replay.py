9cacc05691c1e7578a561d96e9581024b5a87df7
x0r9 <x0r9@users.noreply.github.com>
2021-12-12 19:25:14 +0000

    use new aprslib to parse and output the positional data

--- src/replay.py
+++ src/replay.py
@@ -1,19 +1,43 @@
 import aprs
 import framecap
+import aprslib
 
-def on_frame(frame):
+out_file = open("out.csv", "w")
+
+def on_frame(ts, frame):
     """
     demo frame decode
     """
-    print("---")
-    a = aprs.Frame(frame)
-    print(a)
-    a = aprs.Frame(frame[1:])
-    print(a)
-    print(dir(a))
-    src = a.source
-    dst = a.destination
-    print(src, dst)
+    #print("---")
+
+    a = aprs.parse_frame(frame)
+    #print(type(a), a)
+    #print(a.source, a.destination, a.path, "!!!", a.info)
+    #print(type(a.info))
+    #print(a.info.data, a.info.data_type, a.info.safe)
+
+    try:
+        par = aprslib.parse(str(a))
+        #print(par)
+    except aprslib.exceptions.ParseError as e:
+        print("No parse")
+        return
+    except aprslib.exceptions.UnknownFormat as e:
+        print("unkown format")
+        return
+
+    # Does it contain all the following?
+    s = f"{ts}, {par['from']}, {par.get('latitude','')}, {par.get('longitude','')}, {par.get('comment', '')}"
+    print(s)
+    out_file.write(s+"\n")
+    #a = aprs.Frame(frame)
+    #print(a)
+    #a = aprs.Frame(frame[1:])
+    #print(a)
+    #print(dir(a))
+    #src = a.source
+    #dst = a.destination
+    #print(src, dst)
 
 if __name__ == "__main__":
     f = open("delete-me.txt", "r")
@@ -24,7 +48,7 @@ if __name__ == "__main__":
     while d is not None:
         ts, frame = d
 
-        print(ts, frame)
-        on_frame(frame)
+        #print(ts, frame)
+        on_frame(ts, frame)
 
         d = reader.read_next()
