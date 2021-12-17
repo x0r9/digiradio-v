c92585bc0202ca33067cf93d310f8f2da0abf368
x0r9 <x0r9@users.noreply.github.com>
2021-12-17 22:35:09 +0000

    view more on map, handle more expctions when reading (bad) data

--- src/aprs_data.py
+++ src/aprs_data.py
@@ -22,18 +22,41 @@ def get_last_points(time_window_sec):
         if ts < ts_after:
             return # too old
 
-        aprs_frame = aprs.parse_frame(frame)
         try:
+            aprs_frame = aprs.parse_frame(frame)
             par = aprslib.parse(str(aprs_frame))
         except (aprslib.exceptions.ParseError, aprslib.exceptions.UnknownFormat) as e:
             return
-
-        # is it a point?
-        lat_lon = [par.get("latitude", None), par.get("longitude", None)]
-        if None in lat_lon:
+        except Exception as e:
             return
 
-        stations[aprs_frame.info] = par
+        # is it a point?
+        curr_lat_lon = [par.get("latitude", None), par.get("longitude", None)]
+        if None in curr_lat_lon:
+            return
+
+        # Determine the object by source or object name...
+        obj_name = par.get("from")
+        if "object_name" in par:
+            obj_name = par.get("object_name").strip()
+
+        # check if there is a history or not?
+        if obj_name in stations:
+            # This already exists!
+            prev_frame = stations[obj_name]
+            prev_lat_lon = [prev_frame.get("latitude", None), prev_frame.get("longitude", None)]
+            old_path = None
+            if curr_lat_lon != prev_lat_lon:
+                # it has moved!
+                old_path = prev_frame.get("moving_path", None)
+                if old_path is None:
+                    old_path = []
+                old_path.append([ts]+prev_lat_lon)
+            if old_path is not None:
+                par["moving_path"] = old_path
+
+
+        stations[obj_name] = par
 
     f = open(file_path, "r")
 
