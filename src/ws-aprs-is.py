fabee57a0ddc87827b02d2bc375ec8dce626b6fd
x0r9 <x0r9@users.noreply.github.com>
2022-10-01 17:32:23 +0100

    basic aprs-is listen script added

---
+++ src/ws-aprs-is.py
@@ -0,0 +1,29 @@
+"""
+Connect to APRS-IS and listen for data to feed into application
+"""
+import argparse
+import aprslib
+import logging
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("ws-aprs-is - send APRS-IS data to a websocket for processing")
+    # parser.add_argument("-ws", type=str, help="websocket address to send data too")
+
+    args = parser.parse_args()
+
+    #kiss.LOG_LEVEL = logging.debug
+    logging.basicConfig(level=logging.DEBUG)
+
+    # Try and connect
+    def p(x): print(x)
+
+    host = "rotate.aprs.net"
+    port = 14580 # this is for filters
+    filter = "r/33.25/-96.5/500"
+
+    AIS = aprslib.IS("N0CALL", host=host, port=port)
+    AIS.filter = filter
+    AIS.connect()
+    AIS.consumer(p, raw=True)
